a2d4e1ead7f45ed4857d37a18a4a8b158d717946
KoSungmin <blur2521@gmail.com>
2024-08-23 00:01:37 +0900

    Create Project

---
+++ NewsApp/NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by blurfree on 8/22/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
